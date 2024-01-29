46c355ca162f010b82e0483af4d79f3ce37f2b56
RyazhenovEO <RyazhenovEO@gmail.com>
2024-01-29 20:37:25 +0300

    delete more

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,11 +5,9 @@ app_name = 'pages'
 
 urlpatterns = [
     path('about/', TemplateView.as_view(
-        template_name='pages/about.html'
-        ),
+        template_name='pages/about.html'),
         name='about'),
     path('rules/', TemplateView.as_view(
-        template_name='pages/rules.html'
-        ),
+        template_name='pages/rules.html'),
         name='rules'),
 ]
